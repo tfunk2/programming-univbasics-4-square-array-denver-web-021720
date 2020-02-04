b39fdb3fdc70996923d8977f84b2c3f003b769d9
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-04 20:00:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
   counter = 0
+  new_array = []
   while array[counter] do 
-    puts array[counter]**
+    puts new_array.insert(-1, array[counter]**2)
+    counter += 1
+    new_array
 end
